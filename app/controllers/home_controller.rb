65da8c37faf20b7aa5e216b33ca09afea6c85739
Jay Adkisson <jjmadkisson@gmail.com>
2011-08-28 09:37:53 -0700

    redirect to new_session_path if not logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
     if current_account.logged_in?
       redirect_to plan_path(current_account.login)
+    else
+      redirect_to new_session_path
     end
   end
 end
