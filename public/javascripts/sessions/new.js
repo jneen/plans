7b1730c29d5f31d8dbb5e8d4cc7a77e3d193b3b6
Jay Adkisson <jjmadkisson@gmail.com>
2011-08-29 13:23:24 -0700

    a cute little cryptographic hash robot

---
+++ public/javascripts/sessions/new.js
@@ -0,0 +1,21 @@
+Plans.Robohash = (function($) {
+  function hash(text) {
+    return (new jsSHA(text, 'ASCII')).getHash('SHA-256', 'HEX');
+  }
+
+  function robohashUrl(text) {
+    return 'http://robohash.org/'+text+'.png?size=50x50';
+  }
+
+  function init() {
+    var passwordField = $('#login input[type=password]')
+      , img = $('#robohash img');
+    ;
+    
+    passwordField.keyup(function() {
+      img.attr('src', robohashUrl(hash(passwordField.val())));
+    });
+  }
+
+  $(document).ready(init);
+})(jQuery);
