c99633dcd4296ddbf4c4e0ed8d6b598c10049a34
Jay Adkisson <jjmadkisson@gmail.com>
2011-09-13 17:48:11 -0700

    allow hr tags

--- app/models/text_filter.rb
+++ app/models/text_filter.rb
@@ -1,6 +1,18 @@
 class TextFilter < Redcarpet::Render::HTML
   def postprocess(doc)
     # clean up the doc to prevent xss stuff
-    Sanitize.clean(doc, Sanitize::Config::RELAXED)
+    Sanitize.clean(doc, sanitize_config)
+  end
+
+private
+  def sanitize_config
+    @sanitize_config ||= begin
+      config = Sanitize::Config::RELAXED.dup
+      config[:elements] = config[:elements].dup
+
+      config[:elements] << 'hr'
+
+      config
+    end
   end
 end
